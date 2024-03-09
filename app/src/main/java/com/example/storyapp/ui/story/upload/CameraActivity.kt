63db4ab0d2c1cfb00ee280c22a630ff8d2fc71fa
N H Ausi <oz.ausi@gmail.com>
2024-03-09 19:14:13 +0700

    change location service from CameraActivity  to UploadStoryActivity

--- app/src/main/java/com/example/storyapp/ui/story/upload/CameraActivity.kt
+++ app/src/main/java/com/example/storyapp/ui/story/upload/CameraActivity.kt
@@ -1,20 +1,15 @@
 package com.example.storyapp.ui.story.upload
 
-import android.Manifest
 import android.content.Intent
-import android.content.IntentSender
 import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Build
 import android.os.Bundle
-import android.os.Looper
 import android.util.Log
 import android.view.OrientationEventListener
 import android.view.Surface
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Toast
-import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
@@ -25,16 +20,6 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import com.example.storyapp.databinding.ActivityCameraBinding
 import com.example.storyapp.ui.story.createCustomTempFile
-import com.google.android.gms.common.api.ResolvableApiException
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.LocationSettingsRequest
-import com.google.android.gms.location.Priority
-import com.google.android.gms.maps.model.LatLng
-import java.util.concurrent.TimeUnit
 
 class CameraActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraBinding
@@ -42,38 +27,18 @@ class CameraActivity : AppCompatActivity() {
     private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
     private var imageCapture: ImageCapture? = null
 
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var locationRequest: LocationRequest
-    private lateinit var locationCallback: LocationCallback
-    private var isTracking = false
-    private var lastLatLng: Location? = null
-
     private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) { permissions ->
-        when {
-            permissions[Manifest.permission.ACCESS_FINE_LOCATION] ?: false -> {
-                // Precise location access granted.
-                getMyLastLocation()
-            }
-
-            permissions[Manifest.permission.ACCESS_COARSE_LOCATION] ?: false -> {
-                // Only approximate location access granted.
-                getMyLastLocation()
-            }
-
-            permissions[Manifest.permission.CAMERA] ?: false -> {
-                startCamera()
-            }
-
-            else -> {
-                // No location access granted.
-            }
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "Permission request granted", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "Permission request denied", Toast.LENGTH_LONG).show()
         }
     }
 
     private fun allPermissionsGranted() = ContextCompat.checkSelfPermission(
-        this, REQUIRED_PERMISSION.toString()
+        this, REQUIRED_PERMISSION
     ) == PackageManager.PERMISSION_GRANTED
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -83,11 +48,7 @@ class CameraActivity : AppCompatActivity() {
 
         if (!allPermissionsGranted()) {
             requestPermissionLauncher.launch(REQUIRED_PERMISSION)
-            createLocationRequest()
-            createLocationCallback()
         }
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         binding.switchCamera.setOnClickListener {
             cameraSelector =
@@ -96,26 +57,12 @@ class CameraActivity : AppCompatActivity() {
             startCamera()
         }
         binding.captureImage.setOnClickListener { takePhoto() }
-        binding.locationSwitch.setOnCheckedChangeListener { _, isChecked ->
-            isTracking = isChecked
-            if (isTracking) {
-                startLocationUpdate()
-            } else stopLocationUpdate()
-        }
     }
 
     public override fun onResume() {
         super.onResume()
         hideSystemUI()
         startCamera()
-        if (isTracking) {
-            startLocationUpdate()
-        } else stopLocationUpdate()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        stopLocationUpdate()
     }
 
     private fun startCamera() {
@@ -141,29 +88,11 @@ class CameraActivity : AppCompatActivity() {
         }, ContextCompat.getMainExecutor(this))
     }
 
-    private fun startLocationUpdate() {
-        try {
-            fusedLocationClient.requestLocationUpdates(
-                locationRequest, locationCallback, Looper.getMainLooper()
-            )
-        } catch (exception: SecurityException) {
-            Log.e(TAG, "Error : " + exception.message)
-        }
-    }
-
-    private fun stopLocationUpdate() {
-        fusedLocationClient.removeLocationUpdates(locationCallback)
-    }
-
     private fun takePhoto() {
         val imageCapture = imageCapture ?: return
         val photoFile = createCustomTempFile(application)
-        val metadata = ImageCapture.Metadata().apply {
-            location = lastLatLng
-        }
-        Log.d(TAG, "takePhoto: $lastLatLng")
         val outputOptions =
-            ImageCapture.OutputFileOptions.Builder(photoFile).setMetadata(metadata).build()
+            ImageCapture.OutputFileOptions.Builder(photoFile).build()
         imageCapture.takePicture(outputOptions,
             ContextCompat.getMainExecutor(this),
             object : ImageCapture.OnImageSavedCallback {
@@ -171,9 +100,6 @@ class CameraActivity : AppCompatActivity() {
                     val intent = Intent()
                     intent.putExtra(EXTRA_CAMERAX_IMAGE, output.savedUri.toString())
                     setResult(CAMERAX_RESULT, intent)
-                    intent.putExtra(EXTRA_LATITUDE, lastLatLng?.latitude)
-                    intent.putExtra(EXTRA_LONGITUDE, lastLatLng?.longitude)
-
                     Toast.makeText(
                         this@CameraActivity, "Berhasil mengambil gambar.", Toast.LENGTH_SHORT
                     ).show()
@@ -214,88 +140,6 @@ class CameraActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         orientationEventListener.disable()
-        stopLocationUpdate()
-    }
-
-    private val resolutionLauncher = registerForActivityResult(
-        ActivityResultContracts.StartIntentSenderForResult()
-    ) { result ->
-        when (result.resultCode) {
-            RESULT_OK -> Log.i(TAG, "onActivityResult: All location settings are satisfied.")
-
-            RESULT_CANCELED -> Toast.makeText(
-                this,
-                "Anda harus mengaktifkan GPS untuk menggunakan aplikasi ini!",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-    }
-
-    private fun createLocationRequest() {
-        locationRequest =
-            LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, TimeUnit.SECONDS.toMillis(1))
-                .setIntervalMillis(TimeUnit.SECONDS.toMillis(1))
-                .setMaxUpdateDelayMillis(TimeUnit.SECONDS.toMillis(1))
-                .setWaitForAccurateLocation(true).build()
-
-        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
-        val client = LocationServices.getSettingsClient(this)
-        client.checkLocationSettings(builder.build()).addOnSuccessListener {
-            getMyLastLocation()
-        }.addOnFailureListener {
-            if (it is ResolvableApiException) {
-                try {
-                    resolutionLauncher.launch(
-                        IntentSenderRequest.Builder(it.resolution).build()
-                    )
-                } catch (sendEx: IntentSender.SendIntentException) {
-                    Toast.makeText(this, sendEx.message, Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
-
-    private fun checkPermission(permission: String): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this, permission
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun getMyLastLocation() {
-        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION) && checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)) {
-            fusedLocationClient.lastLocation.addOnSuccessListener {
-                if (it != null) {
-                    startLocation(it)
-                } else {
-                    Toast.makeText(this, "Location not Found. Try Again", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-        } else {
-            requestPermissionLauncher.launch(
-                arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                )
-            )
-        }
-    }
-
-    private fun startLocation(location: Location): LatLng {
-        return LatLng(location.latitude, location.longitude)
-    }
-
-    private fun createLocationCallback() {
-        locationCallback = object : LocationCallback() {
-            override fun onLocationResult(p0: LocationResult) {
-                for (location in p0.locations) {
-                    Log.d(TAG, "onLocationResult: ${location.latitude} ${location.longitude}")
-                    Log.d(TAG, "onLocationResult: $location")
-                    lastLatLng = location
-                }
-            }
-        }
     }
 
     private fun hideSystemUI() {
@@ -313,14 +157,8 @@ class CameraActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "CameraActivity"
         const val EXTRA_CAMERAX_IMAGE = "CameraX Image"
-        const val EXTRA_LATITUDE = "CameraX Latitude"
-        const val EXTRA_LONGITUDE = "CameraX Longitude"
         const val CAMERAX_RESULT = 200
-        private val REQUIRED_PERMISSION = arrayOf(
-            "android.permission.CAMERA",
-            "android.permission.ACCESS_FINE_LOCATION",
-            "android.permission.ACCESS_COARSE_LOCATION"
-        )
+        private const val REQUIRED_PERMISSION = "android.permission.CAMERA"
     }
 
 
--- app/src/main/java/com/example/storyapp/ui/story/upload/UploadStoryActivity.kt
+++ app/src/main/java/com/example/storyapp/ui/story/upload/UploadStoryActivity.kt
@@ -1,30 +1,45 @@
 package com.example.storyapp.ui.story.upload
 
+import android.Manifest
 import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.location.Location
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
 import com.example.storyapp.R
 import com.example.storyapp.ViewModelFactory
 import com.example.storyapp.databinding.ActivityUploadStoryBinding
 import com.example.storyapp.ui.customview.LoadingDialogFragment
 import com.example.storyapp.ui.main.MainActivity
+import com.example.storyapp.ui.story.reduceFileImage
 import com.example.storyapp.ui.story.upload.CameraActivity.Companion.CAMERAX_RESULT
 import com.example.storyapp.ui.story.upload.CameraActivity.Companion.EXTRA_CAMERAX_IMAGE
-import com.example.storyapp.ui.story.reduceFileImage
-import com.example.storyapp.ui.story.upload.CameraActivity.Companion.EXTRA_LATITUDE
-import com.example.storyapp.ui.story.upload.CameraActivity.Companion.EXTRA_LONGITUDE
 import com.example.storyapp.ui.story.uriToFile
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.model.LatLng
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
+import java.util.concurrent.TimeUnit
 
 class UploadStoryActivity : AppCompatActivity() {
 
@@ -33,9 +48,14 @@ class UploadStoryActivity : AppCompatActivity() {
         ViewModelFactory.getInstance(this, application)
     }
     private var currentImageUri: Uri? = null
-    private var currentLatitude: Float? = null
-    private var currentLongitude: Float? = null
     private val loadingDialog = LoadingDialogFragment()
+
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private var isTracking = false
+    private var lastLatLng: Location? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,8 +79,154 @@ class UploadStoryActivity : AppCompatActivity() {
             launcherIntentCameraX.launch(intent)
         }
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        getMyLastLocation()
+        createLocationRequest()
+        createLocationCallback()
+        binding.locationSwitch.setOnCheckedChangeListener { _, isChecked ->
+            isTracking = isChecked
+            if (isTracking) {
+                startLocationUpdate()
+                binding.ivLocation.setColorFilter(R.color.pruple)
+            } else {
+                stopLocationUpdate()
+                binding.ivLocation.setColorFilter(R.color.gray)
+                binding.tvLatitude.text = getString(R.string.latitude)
+                binding.tvLongitude.text = getString(R.string.longitude)
+            }
+        }
+        Log.d(TAG, "onCreate: $lastLatLng")
         binding.uploadButton.setOnClickListener { uploadImage() }
 
+    }
+
+    private fun stopLocationUpdate() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+
+    private fun startLocationUpdate() {
+        try {
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest, locationCallback, Looper.getMainLooper()
+            )
+        } catch (exception: SecurityException) {
+            Log.e(TAG, "Error : " + exception.message)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (isTracking) {
+            startLocationUpdate()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdate()
+    }
+
+    private fun createLocationCallback() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                for (location in p0.locations) {
+                    Log.d(TAG, "onLocationResult: ${location.latitude} ${location.longitude}")
+                    lastLatLng = location
+                    binding.tvLatitude.text =
+                        getString(R.string.latitude_value, location.latitude.toString())
+                    binding.tvLongitude.text =
+                        getString(R.string.longitude_value, location.longitude.toString())
+                }
+            }
+        }
+    }
+
+
+    private val resolutionLauncher = registerForActivityResult(
+        ActivityResultContracts.StartIntentSenderForResult()
+    ) { result ->
+        when (result.resultCode) {
+            RESULT_OK -> Log.i(TAG, "onActivityResult: All location settings are satisfied.")
+
+            RESULT_CANCELED -> Toast.makeText(
+                this, "Anda harus mengaktifkan GPS untuk menggunakan fitur ini!", Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun createLocationRequest() {
+        locationRequest =
+            LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, TimeUnit.SECONDS.toMillis(1))
+                .setIntervalMillis(TimeUnit.SECONDS.toMillis(1))
+                .setMaxUpdateDelayMillis(TimeUnit.SECONDS.toMillis(1))
+                .setWaitForAccurateLocation(true).build()
+
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+        val client = LocationServices.getSettingsClient(this)
+        client.checkLocationSettings(builder.build()).addOnSuccessListener {
+            getMyLastLocation()
+        }.addOnFailureListener {
+            if (it is ResolvableApiException) {
+                try {
+                    resolutionLauncher.launch(
+                        IntentSenderRequest.Builder(it.resolution).build()
+                    )
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    Toast.makeText(this, sendEx.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions[Manifest.permission.ACCESS_FINE_LOCATION] ?: false -> {
+                // Precise location access granted.
+                getMyLastLocation()
+            }
+
+            permissions[Manifest.permission.ACCESS_COARSE_LOCATION] ?: false -> {
+                // Only approximate location access granted.
+                getMyLastLocation()
+            }
+
+            else -> {
+                // No location access granted.
+            }
+        }
+    }
+
+    private fun checkPermission(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this, permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getMyLastLocation() {
+        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION) && checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            fusedLocationClient.lastLocation.addOnSuccessListener {
+                if (it != null) {
+                    startLocation(it)
+                } else {
+                    Toast.makeText(this, "Location not Found. Try Again", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        } else {
+            requestPermissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+    }
+
+    private fun startLocation(location: Location): LatLng {
+        return LatLng(location.latitude, location.longitude)
     }
 
     private val launcherGallery = registerForActivityResult(
@@ -78,8 +244,6 @@ class UploadStoryActivity : AppCompatActivity() {
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it.resultCode == CAMERAX_RESULT) {
                 currentImageUri = it.data?.getStringExtra(EXTRA_CAMERAX_IMAGE)?.toUri()
-                currentLatitude = it.data?.getDoubleExtra(EXTRA_LATITUDE, 100.0)?.toFloat()
-                currentLongitude = it.data?.getDoubleExtra(EXTRA_LONGITUDE, 190.0)?.toFloat()
                 showImage()
             }
         }
@@ -101,8 +265,14 @@ class UploadStoryActivity : AppCompatActivity() {
             val multipartBody = MultipartBody.Part.createFormData(
                 "photo", imageFile.name, requestImageFile
             )
-            val lat = if (currentLatitude == 100F) null else currentLatitude
-            val long = if (currentLongitude == 190F) null else currentLongitude
+
+            val lat = if (binding.tvLatitude.text.toString() == getString(R.string.latitude)) {
+                null
+            } else binding.tvLatitude.text.toString().toFloat()
+            val long = if (binding.tvLongitude.text.toString() == getString(R.string.longitude)) {
+                null
+            } else binding.tvLongitude.text.toString().toFloat()
+
             viewModel.getSession().observe(this) {
                 viewModel.uploadStory(multipartBody, requestBody, lat, long)
             }
@@ -128,5 +298,9 @@ class UploadStoryActivity : AppCompatActivity() {
 
     private fun showError(errorMsg: String?) {
         Toast.makeText(this, "Error! \n$errorMsg", Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        const val TAG = "UploadStoryActivity"
     }
 }
